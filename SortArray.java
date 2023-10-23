286e766ab129c01a4ab1edb27e02ac0d290f77ee
DongkwanKim07 <kim00449@algonquinlive.com>
2023-10-23 13:07:06 -0400

    for github check

--- SortArray.java
+++ SortArray.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class SortArray {
     public static void main(String[] are){
-        int[] arr = {5,2,9,1,5,1,3,8,7};
+        int[] arr = {5,2,9,1,5,1,3,8,7,100};
         Arrays.sort(arr); // Sorts the array in ascending order
         
         // Print out the sorted array
