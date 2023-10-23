aeda9c6e7533f8004879817acf98869bacc14c96
DongkwanKim07 <kim00449@algonquinlive.com>
2023-10-23 12:14:16 -0400

    SortArray Practice

---
+++ SortArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] are){
+        int[] arr = {5,2,9,1,5,1,3,8,7};
+        Arrays.sort(arr); // Sorts the array in ascending order
+        
+        // Print out the sorted array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        }
+}
